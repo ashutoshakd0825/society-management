cce9f05bfc40cd9ae4bc42dd49d5426ba704864e
ashutoshakd0825 <ashutoshakd0825@gmail.com>
2025-09-15 17:16:38 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,12 @@
 const { Pool } = require("pg");
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL ||
-    "postgresql://mangla_landmark_db_user:JmRg71RdnCpKHnRMK7mvADnucUbhAW9Z@dpg-d2tcj17diees7384j7mg-a/mangla_landmark_db"
-     ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
-});
-module.exports = pool;
 
+const pool = new Pool({
+  connectionString:
+    process.env.DATABASE_URL ||
+    "postgresql://mangla_landmark_db_user:JmRg71RdnCpKHnRMK7mvADnucUbhAW9Z@dpg-d2tcj17diees7384j7mg-a/mangla_landmark_db",
+  ssl: process.env.DATABASE_URL
+    ? { rejectUnauthorized: false } // in production (Render)
+    : false                         // in local dev
+});
+
+module.exports = pool;
