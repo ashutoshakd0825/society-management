1a777a02d868d597f05bcdcde8c0db5356759c49
ashutoshakd0825 <ashutoshakd0825@gmail.com>
2025-09-15 17:13:24 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL ||
     "postgresql://mangla_landmark_db_user:JmRg71RdnCpKHnRMK7mvADnucUbhAW9Z@dpg-d2tcj17diees7384j7mg-a/mangla_landmark_db"
-     ssl: { rejectUnauthorized: false }
+     ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 module.exports = pool;
 
