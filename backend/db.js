ecf56ba3a72b5192aaa95c9ede2e7e93e6c29c75
ashutoshakd0825 <ashutoshakd0825@gmail.com>
2025-09-15 15:59:39 +0530

    Create db.js

---
+++ backend/db.js
@@ -0,0 +1,8 @@
+const { Pool } = require("pg");
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL ||
+    "postgresql://mangla_landmark_db_user:JmRg71RdnCpKHnRMK7mvADnucUbhAW9Z@dpg-d2tcj17diees7384j7mg-a.oregon-postgres.render.com/mangla_landmark_db",
+  ssl: { rejectUnauthorized: false }
+});
+module.exports = pool;
+
